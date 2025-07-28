f4aaff6e557e44f3df0873596de5d57804c16dec
rodrigo rizzo <rizzo.rodrigo@estudante.ufjf.br>
2025-07-28 19:46:11 -0300

    corrige lista html e index

--- lista.js
+++ lista.js
@@ -1,7 +1,14 @@
-const lista = ["Um", "Dois", "Tres"];
+const lista = ["Um", "Dois", "Três"];
+
 export function getLista(){
     return structuredClone(lista);
 }
+
 export function limpaLista(){
     lista.splice(0);
+}
+export function adicionaItem(item){
+    if(item && item.trim() !== ""){
+        lista.push(item);
+    }
 }
--- main.js
+++ main.js
@@ -1,29 +1,32 @@
 
 
-
-import { getLista, limpaLista } from "./lista";
+import { getLista, limpaLista, adicionaItem } from "./lista.js";
 
 const olItens = document.querySelector("#itens");
 const pEntrada = document.querySelector("#entrada");
 const btnAdicionar = document.querySelector("#adicionar");
 const btnLimpar = document.querySelector("#limpar");
 
-btnLimpar.addEventListener('click', limparItensDeLista);
 
+btnAdicionar.addEventListener('click', () => {
+    const item = pEntrada.textContent.trim();
+    adicionaItem(item);
+    pEntrada.textContent = "";
+    atualizarLista();
+});
+btnLimpar.addEventListener('click', limparItensDeLista);
 function limparItensDeLista(){
     limpaLista();
     atualizarLista();
 }
 
 atualizarLista();
-
 function atualizarLista(){
     olItens.innerHTML = "";
     let lista = getLista();
-    for(let i=0; i< lista.length; i++)
-        {
+    for(let i=0; i< lista.length; i++){
         const li = document.createElement('li');
         li.textContent = lista[i];
         olItens.appendChild(li);
-        }
     }
+}
