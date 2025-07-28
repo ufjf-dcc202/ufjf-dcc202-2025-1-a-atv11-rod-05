bead9f0badfdcbb8c01dd27eb3822a5d245d3ae2
rodrigo rizzo <rizzo.rodrigo@estudante.ufjf.br>
2025-07-28 19:37:35 -0300

    cria lista main e index e os preenche

---
+++ lista.js
@@ -0,0 +1,7 @@
+const lista = ["Um", "Dois", "Tres"];
+export function getLista(){
+    return structuredClone(lista);
+}
+export function limpaLista(){
+    lista.splice(0);
+}
---
+++ main.js
@@ -0,0 +1,27 @@
+
+
+
+import { getLista, limpaLista } from "./lista";
+
+const olItens = document.querySelector("#itens");
+const pEntrada = document.querySelector("#entrada");
+const btnAdicionar = document.querySelector("#adicionar");
+const btnLimpar = document.querySelector("#limpar");
+
+btnLimpar.addEventListener('click', limparItensDeLista);
+
+function limparItensDeLista(){
+    limpaLista();
+    atualizarLista();
+}
+
+atualizarLista();
+
+function atualizarLista(){
+    olItens.innerHTML = "";
+    let lista = getLista();
+    for(let i=0; i< lista.length; i++){
+        const li = document.createElement('li');
+        li.textContent = lista[i];
+        olItens.appendChild(li);
+    }
